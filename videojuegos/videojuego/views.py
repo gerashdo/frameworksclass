2ac33ce3bda4659d2a3c03422d9c849623a7d2b0
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-02-23 11:14:13 -0600

    Agregado agregar en categorias

--- videojuegos/videojuego/views.py
+++ videojuegos/videojuego/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, redirect
 from .models import Categoria, Videojuego
+from .form_categoria import CategoriaForm
 
 # Create your views here.
 
-def lista_categorias(request):
+def lista_categoria(request):
     categorias= Categoria.objects.all()
     return render(request,'lista_categorias.html',{'categorias':categorias})
 
 def eliminar_categoria(request, id):
     categoria = Categoria.objects.get(id=id)
     categoria.delete()
-    return redirect('videojuego:lista_categorias')
+    return redirect('categoria:lista_categoria')
+
+def nuevo_categoria(request):
+    form = CategoriaForm
+
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categoria:lista_categoria')
+
+    context = {
+        'form' : form
+    }
+    return render(request,'nuevo_categoria.html',context )
+
 
 def lista_videojuegos(request):
     videojuegos = Videojuego.objects.all()
--- videojuegos/videojuegos/urls.py
+++ videojuegos/videojuegos/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('videojuegos/', include('videojuego.urls')),
+    path('categorias/', include('videojuego.urls_categoria')),
 ]
